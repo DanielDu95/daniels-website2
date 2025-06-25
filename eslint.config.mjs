461d3f779a23fa884c8fa2127e9b431fd8c158d0
Daniel Du <daniel@DanieldeMacBook-Air.local>
2025-06-25 21:55:28 +0800

    warn no-unused-vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "warn", // Set no-unused-vars to warn
+      "no-explicit-any": "warn", // Set no-explicit-any to warn
+    },
+  },
 ];
 
 export default eslintConfig;
